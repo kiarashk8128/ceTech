94b0fa1e75b6eee3d139d8a318bb42588465cb3b
unknown <oxfvard@gmail.com>
2023-06-01 10:33:10 +0330

    second

--- predictor.v
+++ predictor.v
@@ -1,8 +1,5 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
 
-  // Define the necessary parameters
-  parameter COUNTER_BITS = 2; // Number of bits for the counter
-  parameter SATURATION_THRESHOLD = (1 << COUNTER_BITS) - 1; // Threshold for saturation
 
   // Declare the input and output signals
   input request; // Input signal representing branch request
@@ -12,12 +9,12 @@ module predictor(input wire request, result, clk, taken, output reg prediction);
   output reg prediction; // Output signal representing the predicted outcome
 
   // Define the saturating counter variable
-  reg [COUNTER_BITS-1:0] saturating_counter = 0;
+  reg [1:0] saturating_counter = 0;
 
   // Implement the saturating counter logic
   always @(posedge clk) begin
     if (result) begin
-      if (taken && (saturating_counter < SATURATION_THRESHOLD))
+      if (taken && (saturating_counter < 3))
         saturating_counter <= saturating_counter + 1;
       else if (!taken && (saturating_counter > 0))
         saturating_counter <= saturating_counter - 1;
@@ -27,7 +24,7 @@ module predictor(input wire request, result, clk, taken, output reg prediction);
   // Determine the prediction based on the saturating counter
   always @(posedge clk) begin
     if (request)
-      prediction <= (saturating_counter >= (SATURATION_THRESHOLD >> 1)) ? 1 : 0;
+      prediction <= saturating_counter[1];
   end
 
 endmodule
